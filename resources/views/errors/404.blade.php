3b1fd501ab448824aa012b38497440a63c5eb8a2
rmit-s3516930-yuxuan-cheng <s3516930@student.rmit.edu.au>
2018-09-08 15:16:12 +1000

    Update 404.blade.php

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -14,17 +14,14 @@
             @include('layouts.partials.nav')
         <main>
             <div class = "container">
-            <br>
-            <div class="row">
-                <div class="col-8">
-                    <img src="./css/images/404.gif" width="440px">
-                </div>
-                <div class="col-4" style="margin: 0; position: absolute; top: 40%; left: 50%;">
-                    <h2>ooops, page not found</h2>
-                    <p>
-                        Back to <a href="">homepage</a>
-                    </p>
-                </div>
+                <br>
+                <div class="row">
+                    <div class="col-8">
+                        <img src="./css/images/404.gif" width="440px">
+                    </div>
+                    <div class="col-4" style="margin-top: 10%; text-shadow: -2px -2px 0 white,2px -2px 0 white,-2px 2px 0 white,2px 2px 0 white;">
+                        <h2>ooops, page not found</h2>
+                    </div>
                 </div>
             </div>
 
