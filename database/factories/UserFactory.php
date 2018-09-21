0fc09cdee882cd6a35e51014200fcdcd98b0c0bb
Omer Khan <s3466908@student.rmit.edu.au>
2018-09-21 18:03:07 +1000

    php Unit Testing For Login and Registration

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,9 +15,15 @@ use Faker\Generator as Faker;
 
 $factory->define(App\User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+        'first_name' => 'FN',
+        'last_name' => 'LN',
+        'date_of_birth' => '1993-06-08',
+        'contact_number' => '0478174422',
+        'licence_number' => '0009870534',
+        'terms' => '1',
+        'is_activated' => '1',
         'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
+        'password' => Hash::make('123Password') // secret
     ];
 });
