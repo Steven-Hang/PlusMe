8345f7ee18c123e26a5f418c01f0e3640629ef38
rmit-s3516930-yuxuan-cheng <s3516930@student.rmit.edu.au>
2018-10-05 14:36:06 +1000

    reset password page update

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}" aria-label="{{ __('Reset Password') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+<div class="container-reset">
+    <div class="wrap-reset">
+        <div class="reset-form-title" >
+            <span class="reset-form-title-1">
+                Reset password
+            </span>
         </div>
+        <!-- Display Session Status -->
+        @if (session('status'))
+            <div class="alert alert-success" role="alert">
+                {{ session('status') }}
+            </div>
+        @endif
+
+        <form method="POST" class="reset-form validate-form" action="{{ route('password.email') }}" aria-label="{{ __('Reset Password') }}">
+        @csrf
+            <div class="wrap-input validate-input" data-validate="Username is required">
+
+                    <span class="label-input">{{ __('E-Mail') }}</span>
+                    <input id="email" type="email" class="input form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                    @if ($errors->has('email'))
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $errors->first('email') }}</strong>
+                        </span>
+                    @endif
+                    <span class="focus-input"></span>
+            </div>
+            <div class="container-reset-form-btn pt-4">
+                <button type="submit" class="reset-form-btn">
+                    {{ __('Send Password Reset Link') }}
+                </button>
+            </div>
+        </form>
     </div>
 </div>
+
+
 @endsection
--- resources/views/user/messages.blade.php
+++ resources/views/user/messages.blade.php
@@ -4,12 +4,9 @@
 <div class="container">
 
         <div class="wrapper">
-        @include('layouts.partials.sidebar')
     <div class="my-3 p-3 bg-white rounded box-shadow">
         <h6 class="border-bottom border-gray pb-2 mb-0">Message</h6>
-        <small class="d-block text-right mt-3">
-            <a href="#">Message to friend</a>
-          </small>
+
         <div class="media text-muted pt-3">
           <img data-src="holder.js/32x32?theme=thumb&bg=007bff&fg=007bff&size=1" alt="" class="mr-2 rounded">
           <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
@@ -33,18 +30,18 @@
             Welcome!!...
           </p>
         </div>
-        
+
         @foreach($data as $msg)
-        
+
         {{$msg->subject}}
         <br>
         {{$msg->message}}
 
         <hr>
         @end
-        
+
       </div>
-        
+
 
         </div>
     @include('layouts.partials.footer')
