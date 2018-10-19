73c55b13392b772aae2d30a49c14541b89182521
rmit-s3516930-yuxuan-cheng <s3516930@student.rmit.edu.au>
2018-10-19 12:54:25 +1100

    add sidebar to layout partial

--- resources/views/messenger/create.blade.php
+++ resources/views/messenger/create.blade.php
@@ -1,27 +1,51 @@
-@extends('layouts.app')
+@include('layouts.partials.head')
 
-@section('content')
-    <h1>Create a new message</h1>
-    <form action="{{ route('messages.store') }}" method="post">
-        {{ csrf_field() }}
-        <div class="col-md-6">
-            <!-- Subject Form Input -->
-            <div class="form-group">
-                <label class="control-label">Subject</label>
-                <input type="text" class="form-control" name="subject" placeholder="Subject"
-                       value="{{ old('subject') }}">
+<body id="page-top">
+    @include('layouts.partials.nav')
+    <div id="wrapper">
+        @include('layouts.partials.adminsidebar')
+        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
+            <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom border-dark">
+                <h1 class="h1">Create a new notification</h1>
+            </div>
+            <div class="wrap-createmsg mx-auto">
+                <form action="{{ route('messages.store') }}" method="post" class="createmsg-form validate-form flex-sb flex-w mt-4 text-center">
+
+                    {{ csrf_field() }}
+
+                    <!-- Subject Form Input -->
+                    <div class="message-label pb-3 ">
+                            Subject
+                    </div>
+                    <div class="wrap-message validate-input m-b-36" data-validate = "Username is required">
+                        <input class="message" type="text" name="subject" placeholder="Subject" value="{{ old('subject') }}" >
+                        <span class="focus-message"></span>
+                    </div>
+                    <br>
+                    <!-- Message Form Input -->
+                    <div class="message-label pb-3 ">
+                        Message
+                    </div>
+                    <div class="wrap-message validate-input m-b-12" data-validate = "Password is required">
+                        <textarea name="message"  class="message" height="400px">{{ old('message') }}</textarea>
+                        <span class="focus-message"></span>
+                    </div>
+
+                    <!-- Submit Form Input -->
+                    <div class="container-createmsg-form-btn">
+                        <button class="createmsg-form-btn mt-3">
+                            Send
+                        </button>
+                    </div>
+
+                </form>
             </div>
 
-            <!-- Message Form Input -->
-            <div class="form-group">
-                <label class="control-label">Message</label>
-                <textarea name="message" class="form-control">{{ old('message') }}</textarea>
-            </div>
+    </main>
 
-            <!-- Submit Form Input -->
-            <div class="form-group">
-                <button type="submit" class="btn btn-primary form-control">Submit</button>
-            </div>
-        </div>
-    </form>
-@stop
+</div>
+<!-- /#wrapper -->
+
+</body>
+
+</html>
