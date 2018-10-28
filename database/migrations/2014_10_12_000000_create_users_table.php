f2615987379c7bf11fabfad10f45727030243a4f
StevenHang1994 <36983582+StevenHang1994@users.noreply.github.com>
2018-10-28 18:09:24 +1100

    fixed register made google id field nullable thank god for CI tools

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('google_id');
+            $table->string('google_id')->nullable();
             $table->string('first_name');
             $table->string('last_name');
             $table->date('date_of_birth')->nullable();     
