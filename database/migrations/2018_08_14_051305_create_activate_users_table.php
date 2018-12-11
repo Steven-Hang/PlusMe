6b937a78a9fa000463d65216e041f41de3fa221c
StevenHang1994 <36983582+StevenHang1994@users.noreply.github.com>
2018-12-11 23:32:27 +1100

    fixed small db issue which requires id's for activate_users table

--- database/migrations/2018_08_14_051305_create_activate_users_table.php
+++ database/migrations/2018_08_14_051305_create_activate_users_table.php
@@ -14,6 +14,7 @@ class CreateActivateUsersTable extends Migration
     public function up()
     {
         Schema::create('activate_users', function (Blueprint $table) {
+            $table->increments('id');
             $table->integer('user_id');
             $table->string('token');
             $table->timestamps();
